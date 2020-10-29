c6e6eb2e1e72b8978fb572ef74aaaf2a1f1cacc7
beyondbeyond <zby990920@163.com>
2020-10-30 07:46:23 +0800

    zby...

---
+++ test1/src/main/java/com/yemeng/Test03.java
@@ -0,0 +1,12 @@
+package com.yemeng;
+
+/**
+ * @author zby
+ * @create 2020-10-30 7:45
+ */
+public class Test03 {
+
+    public static void main(String[] args) {
+        System.out.println("Ssssssssssssssssss");
+    }
+}
