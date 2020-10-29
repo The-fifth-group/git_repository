c4cd017567a7c14d785ca71454e8d15933698d77
root <597137047@qq.com>
2020-10-29 20:39:27 +0800

    yang

---
+++ test1/src/main/java/com/yemeng/Test2.java
@@ -0,0 +1,12 @@
+package com.yemeng;
+
+/**
+ * @author swx
+ * @company
+ * @create 2020-10-29 11:20
+ */
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!!");
+    }
+}
