c83189cf2f5cd49824173f1cc504ceea0e8cc63d
夜盟 <YeMen9@github.com>
2020-10-29 11:34:11 +0800

    test1发布项目到github

---
+++ test1/src/test/java/yemeng/Test1.java
@@ -0,0 +1,12 @@
+package yemeng;
+
+/**
+ * @author swx
+ * @company
+ * @create 2020-10-29 11:20
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("Hello,GitHub!");
+    }
+}
